5a646e3802234daf8605febc628db65f009ebd02
Michael Hucka <mhucka@caltech.edu>
2019-05-31 09:47:52 -0700

    Made check_log_file create missing dir if needed

--- admin/utilities.sh
+++ admin/utilities.sh
@@ -9,10 +9,26 @@
 check_log_file() {
     set_ifs
 
-    if [[ -n $HUGO_LOGFILE ]]; then
-        if [[ -e "$HUGO_LOGFILE" && ! -w "$HUGO_LOGFILE" ]]; then
-            printf '%s\n' "Unable to write log file $HUGO_LOGFILE."
-            exit 1
+    if [ -n $HUGO_LOGFILE ]; then
+        # We have a value for the configuration variable.  Does the file exist?
+        if [ ! -e "$HUGO_LOGFILE" ]; then
+            # The file doesn't exist.  Is it because the dir doesn't exist?
+            dir=$(dirname "$HUGO_LOGFILE")
+            if [ ! -d "$dir" ]; then
+                # Try to create the directory.
+                mkdir "$dir"
+                if [ ! -d "$dir" ]; then
+                    echo "Unable to create $dir" >&2
+                    exit 2
+                fi
+            fi
+            if [ ! -w "$dir" ]; then
+                echo "Unable to write to directory $dir" >&2
+                exit 2
+            fi
+        elif [ ! -w "$HUGO_LOGFILE" ]; then
+            echo "Unable to write log file $HUGO_LOGFILE." >&2
+            exit 2
         fi
     fi
 
